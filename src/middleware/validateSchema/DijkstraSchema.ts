b8993fa889a61f6634290a77dca27518c979e7ed
Braum3ister <johannes.uni.kit@gmail.com>
2022-02-18 21:53:51 +0100

    Implemented some Validation

--- src/middleware/validateSchema/DijkstraSchema.ts
+++ src/middleware/validateSchema/DijkstraSchema.ts
@@ -3,8 +3,38 @@ import { body } from "express-validator"
 
 const dijkstraSchema2D = [
     body("height")
-        .isInt()
-        .withMessage("Height must be a number"),
-    body()
+        .exists({
+            checkFalsy: true
+        })
+        .isInt({
+            min : 0
+        })
+        .withMessage("Height must be a number > 0"),
+    body("width")
+        .exists({
+            checkFalsy: true
+        })
+        .isInt({
+            min : 0
+        })
+        .withMessage("Width must be a number > 0"),
 
-]
+    body("start-point")
+        .exists({checkFalsy : true})
+        .custom((value) => {
+            console.log(typeof value)
+            return true
+        }),
+
+
+    body("end-point")
+        .exists({checkFalsy: true}),
+
+
+    body("walls")
+        .exists({
+            checkNull : true
+        })
+];
+
+export {dijkstraSchema2D}
--- src/routes/api/dijkstra.ts
+++ src/routes/api/dijkstra.ts
@@ -1,10 +1,12 @@
 import {Router, Request, Response} from "express"
 import {startDijkstra} from "../../controllers/dijkstra_session"
+import {dijkstraSchema2D} from "../../middleware/validateSchema/DijkstraSchema";
+import {validateRequestDijkstra} from "../../middleware/ValidateRequest";
 
 const router: Router = Router()
 
 
-router.route("/").post(startDijkstra).get((req, res) => res.sendStatus(200))
+router.route("/").post(dijkstraSchema2D, validateRequestDijkstra, startDijkstra).get((req, res) => res.sendStatus(200))
 
 router.get("/dij", ((req: Request, res: Response) => {
     res.send("res")
